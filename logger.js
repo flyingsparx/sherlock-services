ff3b23b57de22af8d0d1e18dee0929079e441e67
Will Webberley <willwebberley@gmail.com>
2015-07-31 14:00:07 +0000

    enabled storage and retrieval on per-experiment basis

--- logger.js
+++ logger.js
@@ -3,19 +3,18 @@ var PORT = 6666;
 var MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb://127.0.0.1:27017/sherlock', function(dberr, db){
   var today = new Date();
-  var collection = db.collection(today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate());
 
   http.createServer(function(request, response){
       response.setHeader("Access-Control-Allow-Origin", "*");
       if(request.method == "GET" && request.url.indexOf("/cards") == 0){
         try{
           var components = request.url.split("/");
-          var year = parseInt(components[2]);
-          var month = parseInt(components[3])-1;
-          var day = parseInt(components[4]);
-          var collection2 = db.collection(year+"-"+month+"-"+day);
-          console.log("Connected to "+year+"-"+month+"-"+day); 
-          var cards = collection2.find().toArray(function(err, docs){
+          var exp_name = components[2];
+          var year = parseInt(components[3]);
+          var month = parseInt(components[4])-1;
+          var day = parseInt(components[5]);
+          var collection = db.collection(exp_name+"-"+year+"-"+month+"-"+day);
+          var cards = collection.find().toArray(function(err, docs){
             if(docs){
               response.end(JSON.stringify(docs));
             }   
@@ -26,14 +25,16 @@ MongoClient.connect('mongodb://127.0.0.1:27017/sherlock', function(dberr, db){
           response.end();
         }
       }
-      else if(request.method == "POST" && request.url == "/cards"){
+      else if(request.method == "POST" && request.url.indexOf("/cards") == 0){
           var body = "";
           request.on("data", function(chunk){
               body += chunk;
           });
           request.on("end", function(){
               try{
-                console.log(body);
+                var components = request.url.split("/");
+                var exp_name = components[2];
+                var collection = db.collection(exp_name+"-"+today.getFullYear()+"-"+today.getMonth()+"-"+today.getDate());
                 collection.insert(JSON.parse(body));
               }
               catch(err){
