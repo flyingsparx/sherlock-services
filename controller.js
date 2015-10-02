91ff964801e1a9e5cc525466fe45d39996c56139
Will Webberley <willwebberley@gmail.com>
2015-10-02 17:43:19 +0000

    fixed a bug and added better logging

--- controller.js
+++ controller.js
@@ -11,75 +11,74 @@ http.createServer(function(request, response){
       return;
     }
     if(request.url == '/' || request.url.indexOf('/?') == 0){
+      console.log(new Date(), 'Homepage');
       var status = url.parse(request.url, true).query.status;
-      console.log(status);
-
       var html = '<html><body><h2>Mycroft</h2><a href="/restart-mycroft">Restart (or just start) Mycroft service</a><br /><a href="/stop-mycroft">Stop Mycroft</a><h2>Verity</h2><a href="/restart-verity">Restart (or just start) Verity service</a><br /><a href="/stop-verity">Stop Verity</a></body>';
       if(status && status != ''){
         html += '<script>window.alert("'+status+'");</script>';
       }
       response.end(html+'</html>');
     }
-    else if(request.url == '/start-mycroft' || request.url == '/restart-mycroft'){
-        console.log("Killing Mycroft");
+    else if(request.url.indexOf('/start-mycroft') == 0 || request.url.indexOf('/restart-mycroft') == 0){
+        console.log(new Date(), "Killing Mycroft");
         exec('tmux kill-session -t mycroft', {cwd: '/home/apps'}, function(err1, stdout1, stderr1){
-            console.log(stdout1);
-            console.log("Launching new Mycroft session.");
+            console.log(new Date(), stdout1);
+            console.log(new Date(), "Launching new Mycroft session.");
             exec('tmux new-session -s mycroft -d', {cwd: '/home/apps'}, function(err, stdout, stderr){
-                console.log(stdout);
-                console.log("Starting Mycroft.");
+                console.log(new Date(), stdout);
+                console.log(new Date(), "Starting Mycroft.");
                 exec('tmux send-keys -t mycroft "node CENode/cenode.js Mycroft" C-m', {cwd: '/home/apps'}, function(e3, st3, std3){
                     if(err){
-                        console.log(err);
+                        console.log(new Date(), err);
                     }
                     if(stderr){
-                        console.log(stderr);
+                        console.log(new Date(), stderr);
                     }
-                    console.log(stdout);
+                    console.log(new Date(), stdout);
                     response.writeHead(301, {Location: '/?status=Mycroft is now running.'});
                     response.end();
                 });
             });
         });
     }
-    else if(request.url == '/stop-mycroft'){
-        console.log("Killing Mycroft");
+    else if(request.url.indexOf('/stop-mycroft') == 0){
+        console.log(new Date(), "Killing Mycroft");
         exec('tmux kill-session -t mycroft', {cwd: '/home/apps'}, function(err1, stdout1, stderr1){
-           console.log('Killed'); 
+           console.log(new Date(), 'Killed'); 
            response.writeHead(301, {Location: '/?status=Stopped Mycroft'});
            response.end();
         });
     }
-    else if(request.url == '/start-verity' || request.url == '/restart-verity'){
-        console.log("Killing Verity");
+    else if(request.url.indexOf('/start-verity') == 0 || request.url.indexOf('/restart-verity') == 0){
+        console.log(new Date(), "Killing Verity");
         exec('tmux kill-session -t verity', {cwd: '/home/apps'}, function(err1, stdout1, stderr1){
-            console.log(stdout1);
-            console.log("Launching new Verity session.");
+            console.log(new Date(), stdout1);
+            console.log(new Date(), "Launching new Verity session.");
             exec('tmux new-session -s verity -d', {cwd: '/home/apps'}, function(err, stdout, stderr){
-                console.log(stdout);
-                console.log("Starting Verity.");
+                console.log(new Date(), stdout);
+                console.log(new Date(), "Starting Verity.");
                 exec('tmux send-keys -t verity "node CENode/cenode.js Verity 7654" C-m', {cwd: '/home/apps'}, function(e3, st3, std3){
                     if(err){
-                        console.log(err);
+                        console.log(new Date(), err);
                     }
                     if(stderr){
-                        console.log(stderr);
+                        console.log(new Date(), stderr);
                     }
-                    console.log(stdout);
+                    console.log(new Date(), stdout);
                     response.writeHead(301, {Location: '/?status=Verity is now running.'});
                     response.end();
                 });
             });
         });
     }
-    else if(request.url == '/stop-verity'){
-        console.log("Killing Verity");
+    else if(request.url.indexOf('/stop-verity') == 0){
+        console.log(new Date(), "Killing Verity");
         exec('tmux kill-session -t verity', {cwd: '/home/apps'}, function(err1, stdout1, stderr1){
-          console.log('Killed'); 
+          console.log(new Date(), 'Killed'); 
           response.writeHead(301, {Location: '/?status=Stopped Verity'});
           response.end();
         });
     }
 }).listen(PORT);
 
-console.log("Control listener running on port "+PORT+".");
+console.log(new Date(), "Control listener running on port "+PORT+".");
