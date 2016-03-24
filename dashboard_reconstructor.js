4fb02d69400de41f38b12eb7aeab08e979f18d77
Will Webberley <willwebberley@gmail.com>
2016-03-24 11:05:10 +0000

    fixed small typo in readme

--- dashboard_reconstructor.js
+++ dashboard_reconstructor.js
@@ -18,7 +18,7 @@ var model = './model.js' // Model to preload KB with (usually contains character
 var lib = require('../CENode/cenode.js');
 var models = require(model);
 var components = require(process.argv[2]);
-var node = new lib.CENode(lib.MODELS.CORE, models.SHERLOCK_CW);
+var node = new lib.CENode(lib.MODELS.CORE, models.SHERLOCK_CORE);
 
 var questions = node.concepts.question.instances;
 
